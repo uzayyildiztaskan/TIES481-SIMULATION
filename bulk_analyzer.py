5f8363ec4f251843f93079b637c5e6b51df5b516
Hans Hartikainen <hanshartikainen@gmail.com>
2024-11-28 10:07:36 +0200

    Initial attempt at blocking time estimation

--- bulk_analyzer.py
+++ bulk_analyzer.py
@@ -9,24 +9,26 @@ print(os.getcwd())
 with open('simulation.results', 'rb') as simfile:
     data = pickle.load(simfile)
 
-def blocking_time_from_data(data):
+def operation_blocking_time_from_data(data):
     
     results = {}
     timer_totals = []
     for simulation in data:
         operation_status = list(zip(simulation['queue_timestamps']['timestamp'], 
-                               simulation['queue_data']['operation']))
+                               simulation['queue_data']['operation'],
+                               simulation['queue_data']['recovery']))
         
         timer_total = 0
         timer_on = False # We assume an empty hospital in the beginning
         previous_timestamp = 0
         for event in operation_status:
             current_timestamp = event[0]
-            persons_in_queue = event[1]
+            persons_in_operation_queue = event[1]
+            persons_in_recovery_queue = event[2]
             if timer_on:
                 timer_total += (current_timestamp - previous_timestamp)
             previous_timestamp = current_timestamp
-            if persons_in_queue: 
+            if persons_in_operation_queue and persons_in_recovery_queue: 
                 timer_on = True
             else:
                 timer_on = False
@@ -43,11 +45,12 @@ def mean_confidence_interval(data, confidence=0.95):
     h = se * scipy.stats.t.ppf((1 + confidence) / 2., n-1)
     return m, m-h, m+h
 
-bt_data = blocking_time_from_data(data)
+bt_data = operation_blocking_time_from_data(data)
 mean_confidence_interval(bt_data)
 mean, lower, upper = mean_confidence_interval(bt_data)
+print("Blocking times:")
 print(mean)
-print(f"{lower}, {upper}")
+print(f"[{lower}, {upper}]")
 
 
     
